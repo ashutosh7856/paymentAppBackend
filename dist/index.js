5eb5e41749e3254791fd78b6004070c565dacdeb
Ashutosh <ashutoshkkr60@gmail.com>
2025-09-15 17:56:20 +0530

    project stracture

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = 3000;
-app.use(express.json());
-app.listen(PORT, () => {
-    console.log(`Server si running on ${PORT}`);
-});
-//# sourceMappingURL=index.js.map
